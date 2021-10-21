46bf21fb5973809073fc2f89fcea7a4c238bda6b
HZ2016-LAPTOP\huang <huangzhengdoc@163.com>
2021-10-21 21:44:48 +0800

    Learn bar control and keyboard control

---
+++ KeyboardControl/KeyboardControl.cpp
@@ -0,0 +1,17 @@
+﻿#include <opencv2/opencv.hpp>
+//#include <cstdio>
+//#include <iostream>
+using namespace cv;
+int main() {
+	Mat img = imread("hz2016.jpg");
+	imshow("hz2016", img);
+	while (1){
+		int c=waitKey(0);
+		if (c == 27)break;
+		else if (c == 49)std::cout << "You enter key1" << std::endl;
+		else if (c == 50)std::cout << "You enter key2" << std::endl;
+		else if (c == 51)std::cout << "You enter key3" << std::endl;
+	}
+	destroyAllWindows();
+	return 0;
+}
---
+++ TrackBar/TrackBar.cpp
@@ -0,0 +1,30 @@
+﻿#include <opencv2/opencv.hpp>
+//#include <cstdio>
+//#include <iostream>
+using namespace cv;
+static void on_lightness(int lightness, void* userdata) {
+	Mat img = *((Mat*)userdata);
+	Mat m = Mat::zeros(img.size(), img.type());
+	Mat src = Mat::zeros(img.size(), img.type());
+	addWeighted(img, 1.0, m, 0, lightness, src);
+	imshow("Image Change", src);
+}
+static void on_contrast(int contrast, void* userdata) {
+	Mat img = *((Mat*)userdata);
+	Mat m = Mat::zeros(img.size(), img.type());
+	Mat src = Mat::zeros(img.size(), img.type());
+	double contrast_v = contrast / 100.0;
+	addWeighted(img, contrast_v, m,0.0,0,src);
+	imshow("Image Change", src);
+}
+int main() {
+	Mat img = imread("hz2016.jpg");
+	namedWindow("Image Change", WINDOW_AUTOSIZE);
+	int max_lightness = 200,lightness = 50;
+	int max_contrast = 200,contrast = 100;
+	createTrackbar("Light:", "Image Change", &lightness, max_lightness, on_lightness,(void*)(&img));
+	createTrackbar("Contrast:", "Image Change", &contrast, max_contrast, on_contrast, (void*)(&img));
+	waitKey(0);
+	destroyAllWindows();
+	return 0;
+}
