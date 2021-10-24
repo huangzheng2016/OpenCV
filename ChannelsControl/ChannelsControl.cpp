369574166af1b992c783fb18020854c78a20b27c
hz2016 <huangzhengdoc@163.com>
2021-10-24 23:56:40 +0800

    Updata channelsControl

---
+++ ChannelsControl/ChannelsControl.cpp
@@ -0,0 +1,19 @@
+﻿#include <opencv2/opencv.hpp>
+#include <vector>
+using namespace cv;
+int main() {
+	Mat img = imread("hz2016.jpg");
+	imshow("hz2016", img);
+	std::vector<Mat>mv;
+	split(img, mv);
+	imshow("Blue", mv[0]);
+	imshow("Green", mv[1]);
+	imshow("Red", mv[2]);
+	Mat src;
+	mv[1] = mv[2] = 0;
+	merge(mv, src);
+	imshow("True Blue", src);
+	waitKey(0);
+	destroyAllWindows();
+	return 0;
+}
