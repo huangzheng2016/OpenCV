5ce759296dfda40d3bfa41c041e55c7f0dff60e4
hz2016 <huangzhengdoc@163.com>
2021-10-26 00:05:05 +0800

    Update some tips about img.Tocopy

--- cvtColorPro/cvtColorPro.cpp
+++ cvtColorPro/cvtColorPro.cpp
@@ -14,6 +14,8 @@ int main() {
 	redback = Scalar(40, 40, 200);//准备填入红色低
 	bitwise_not(ret, ret);//背景取反
 	img.copyTo(redback, ret);//只为不为0的才会被迁移，
+	//image.copyTo(imageROI，mask),作用是把mask和image重叠以后把mask中像素值为0（black）的点对应的image中的点变为透明，而保留其他点。
+	//就是原图img和redback重合，然后mask中是黑色的，redback为透明
 	imshow("RetredBack", redback);
 	waitKey(0);
 	return 0;
