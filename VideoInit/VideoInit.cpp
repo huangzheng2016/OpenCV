ff291a4283eb5ab9fdcaf43b1abefa74b740d6b9
HZ2016-LAPTOP\huang <huangzhengdoc@163.com>
2021-11-04 10:11:10 +0800

    Update Video Init

---
+++ VideoInit/VideoInit.cpp
@@ -0,0 +1,16 @@
+﻿#include <opencv2/opencv.hpp>
+using namespace cv;
+int main() {
+	VideoCapture capture(0);//0也可以换成一段视频
+	Mat frame;
+	while (1) {
+		capture.read(frame);
+		if (frame.empty())break;
+		imshow("Frame", frame);
+		int c = waitKey(1);
+		if (c == 27)break;
+	}
+	waitKey(0);
+	//capture.release();其实会主动放掉，但是多个摄像头要手动放
+	return 0;
+}
