081029d010c36a3904f5eabdffbca960edd1c014
hz2016 <huangzhengdoc@163.com>
2021-11-04 12:47:13 +0800

    no message

---
+++ Push.bat
@@ -0,0 +1,3 @@
+git -c diff.mnemonicprefix=false -c core.quotepath=false --no-optional-locks push -v --tags origin master:master
+git -c diff.mnemonicprefix=false -c core.quotepath=false --no-optional-locks push -v --tags Tencent-opencv master:master
+git -c diff.mnemonicprefix=false -c core.quotepath=false --no-optional-locks push -v --tags Gogs-opencv master:master
